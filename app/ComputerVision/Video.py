81f6fbe437ba4a91d4208f5b3b14909c56bd4db7
Baher <ferventfrost5@gmail.com>
2022-11-26 19:16:06 +0200

    init

---
+++ app/ComputerVision/Video.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+
+class VideoStream:
+    
+    def __init__(self, _VideoPath):
+        self.VideoPath = _VideoPath
+        self.VideoStream = cv.VideoCapture(self.VideoPath)
+
+    def StopStreaming(self):
+        if cv.waitKey(1) & 0xFF == ord('d'):
+            return True
+        return False
+
+    def ReleaseVideoStream(self):
+        self.VideoStream.release()
+
+    def DisplayFrame(self):
+        ret, frame = self.VideoStream.read()
+        cv.imshow("Video", frame)
+
+    def DisplayVideo(self):
+        while self.VideoStream.isOpened():
+            self.DisplayFrame()
+            if self.StopStreaming():
+                break
+
+        self.ReleaseVideoStream()
+
+if __name__=='__main__':
+    o = VideoStream("C:\Baher\FiddleStick.mp4")
+    o.DisplayVideo()
---
+++ app/test.py
@@ -0,0 +1,3 @@
+from app.ComputerVision.Video import VideoStream
+StartStream = VideoStream("C:\Baher\FiddleStick.mp4")
+StartStream.DisplayVideo()
