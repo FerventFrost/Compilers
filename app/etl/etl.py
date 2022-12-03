3b97f5d5a3e4630d751746af93ecbfa60d6250e2
Baher <ferventfrost5@gmail.com>
2022-12-03 23:35:38 +0200

    refactor ETL file

--- app/etl/etl.py
+++ app/etl/etl.py
@@ -1,46 +1,63 @@
 import pandas
+import sqlalchemy
+import regex as re
 
 class compilerAggregationFunction:
     def __init__(self, _data:pandas.DataFramem, _filters:dict) -> None:
         self.Data = _data
-        self.Left = _filters['left']
+        self.left = _filters['left']
         self.right = _filters['right']
 
+        self.AggFunctions = {
+            'or' : lambda : self.Or(),
+            'and' : lambda : self.And(),
+            'like' : lambda : self.Like(),
+            '>' : lambda : self.GreaterThan(),
+            '>=' : lambda : self.GreaterOrEqual(),
+            '<' : lambda : self.LessThan(),
+            '<=' : lambda : self.LessOrEqual(),
+            '==' : lambda : self.Equal(),
+            '!=' : lambda : self.NotEqual(),
+        }
+
     def Or(self):
-        pass
+        self.Data = pandas.concat([self.left, self.right])
+        return self.Data[~ self.Data.index.duplicated(keep='first')]
 
     def And(self):
-        pass
+        self.Data = pandas.merge([self.left, self.right])
+        return self.Data[~ self.Data.index.duplicated(keep='first')]
 
     def Like(self):
-        pass
+        return self.Data[[True if re.match(self.right, str(x)) else False for x in self.Data[self.left]]]
 
     def GreaterThan(self):
-        pass
+        return self.Data[self.Data[self.left] > self.right]
 
     def GreaterOrEqual(self):
-        pass
+        return self.Data[self.Data[self.left] >= self.right]
 
     def LessThan(self):
-        pass
+        return self.Data[self.Data[self.left] < self.right]
 
     def LessOrEqual(self):
-        pass
+        return self.Data[self.Data[self.left] <= self.right]
 
     def Equal(self):
-        pass
+        return self.Data[self.Data[self.left] == self.right]
 
     def NotEqual(self):
-        pass
+        return self.Data[self.Data[self.left] != self.right]
 
 class DataSource:
 
-    def __init__(self, _filePath:str, _data:pandas.DataFrame, _operation:dict) -> None:
+    def __init__(self, _sorucePath:str, _destinationPath:str, _data:pandas.DataFrame, _operation:dict) -> None:
         self.dfName = None
-        self.FilePath = _filePath
+        self.SourcePath = _sorucePath
+        self.DestinationPath = _destinationPath
         self.Data = _data
         self.Operation = _operation
-        # self.Agg = compilerAggregationFunction()
+        self.Agg = compilerAggregationFunction(self.Data, self.Operation['FILTER'])
 
     def extract(self, type = None) -> None:
         raise NotImplementedError
@@ -48,35 +65,107 @@ class DataSource:
     def load(self, type = None) -> None:
         raise NotImplementedError
 
+    # return Filtered Data
+    def getFilter(self):
+        filter = self.Operation['FILTER']['type']
+        return self.Agg.AggFunctions[filter]
+
+    # return specific columns
     def getColumns(self) -> pandas.DataFrame:
-        pass
+        return self.Data.filter(items= self.Operation['COLUMNS'])
 
+    def getDistinct(self) -> pandas.DataFrame:
+        return self.Data.filter(items= self.Operation['COLUMNS'])
+
+    # sort data
     def getOrder(self) -> pandas.DataFrame:
-        pass
+        columnn = self.Operation['ORDER'][0]
+        return self.Data.sort_values(columnn, ascending= self.Operation['ORDER'][1] == 'ASC')
 
+    # return data in a certain range
     def getLimit(self) -> pandas.DataFrame:
-        pass
+        return self.Data[:self.Operation['LIMIT']]
 
 class CsvDS(DataSource):
-    pass
+    
+    def extract(self, type=None) -> pandas.DataFrame:
+        return pandas.read_csv(self.SourcePath)
+
+    def load(self, type=None) -> None:
+        self.Data.to_csv(self.DestinationPath, mode='a')
 
 class SqliteDS(DataSource):
-    pass
+   
+    def extract(self, type=None) -> pandas.DataFrame:
+        DBSoruce = self.SourcePath.split('/')[0]
+        tableName = self.SourcePath.spli('/')[1]
+
+        sqlite_engine = sqlalchemy.create_engine(f'sqlite:///{DBSoruce}')
+        return pandas.read_sql(f'select * from {tableName}', sqlite_engine)
+
+    def load(self, type=None) -> None:
+        DBDestination = self.DestinationPath.split('/')[0]
+        tableName = self.DestinationPath.split('/')[1]
+
+        sqlite_engine = sqlalchemy.create_engine(f'sqlite:///{DBDestination}')
+        self.Data.to_sql(tableName, sqlite_engine, if_exists='append', index=False)
 
 class MssqlDS(DataSource):
-    pass
+    
+    def extract(self, type=None) -> pandas.DataFrame:
+        connection_string = connection_string.split("/")
+        server_name = connection_string[0]
+        db_name = connection_string[1]
+        table_name = connection_string[2]
+
+        mssql_engine = sqlalchemy.create_engine(f'mssql+pyodbc://{server_name}/{db_name}?trusted_connection=yes&driver=SQL+Server+Native+Client+11.0')
+        table = mssql_engine.execute(f"SELECT * FROM {table_name};")
+    
+        data = pandas.DataFrame(table, columns=table.keys())
+        return data
+
+    def load(self, type=None) -> None:
+        connection_string = connection_string.split("/")
+        server_name = connection_string[0]
+        db_name = connection_string[1]
+        table_name = connection_string[2]
+
+        mssql_engine = sqlalchemy.create_engine(f'mssql+pyodbc://{server_name}/{db_name}?trusted_connection=yes&driver=SQL+Server+Native+Client+11.0')
+        self.Data.to_sql(table_name, mssql_engine, if_exists='append', index=False)
 
 class HtmlDS(DataSource):
-    pass
+    
+    def extract(self, type=None) -> pandas.DataFrame:
+        return pandas.read_html(self.SourcePath)
+
+    def load(self, type=None) -> None:
+        self.Data.to_html(self.DestinationPath)
 
 class JsonDF(DataSource):
-    pass
+    
+    def extract(self, type=None) -> pandas.DataFrame:
+        return pandas.read_json(self.SourcePath, orient='records')
+    
+    def load(self, type=None) -> None:
+        self.Data.to_json(self.DestinationPath)
 
 class XmlDS(DataSource):
-    pass
+    
+    def extract(self, type=None) -> pandas.DataFrame:
+        return pandas.read_xml(self.SourcePath)
+
+    def load(self, type=None) -> None:
+        self.Data.to_xml(self.DestinationPath)
 
 class ExcelDS(DataSource):
-    pass
+    
+    def extract(self, type=None) -> pandas.DataFrame:
+        return pandas.read_excel(self.SourcePath)
+
+    def load(self, type=None) -> None:
+        self.Data.to_excel(self.DestinationPath) 
 
 class ConsolDS(DataSource):
-    pass
+
+    def load(self, type=None) -> None:
+        pass
